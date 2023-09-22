cc3f7fb5d7cebbe559c0280ed583b192cc736146
¨Bhuvana¨ <bhuvanaselvaraj25@gmail.com>
2023-09-22 10:20:45 +0200

    Add project files.

---
+++ C#methods-assignment/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+using C_methods_assignment.Services;
+
+//Assignment1
+
+Class1.Method1();
+
+//Assignment2
+
+Class1.Method2();
+
+//Assignment3
+
+string message = "Please enter a number:";
+        string userText = Class1.Method3(message);
+        Console.WriteLine("You entered:" + userText);
+
+//Assignment4
+
+Console.WriteLine("Enter a word: ");
+string word = Console.ReadLine();
+string userInput = Class1.Method4(word);
+Console.WriteLine($"You entered: {userInput}");
+
+//Assignment5
+
+Console.WriteLine("My firstname:");
+string firstName = "Bhuvana";
+Console.WriteLine(firstName);
+Console.WriteLine("My lastname:");
+string lastName = "Selvaraj";
+Console.WriteLine(lastName);
+string fullName = Class1.Method5(firstName, lastName);
---
+++ C#methods-assignment/Services/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_methods_assignment.Services
+{
+    internal class Class1
+    {
+ 
+        
+        public static void Method1()
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+        public static void Method2()
+        {
+
+            Console.WriteLine("Enter a text:");
+            string text = Console.ReadLine();
+            Console.WriteLine("#" + text);
+        }
+
+        public static string Method3(string message)
+        {
+            Console.WriteLine(message);
+            string userInput = Console.ReadLine();
+            return userInput;
+        }
+       
+      
+
+       public static string Method4(string word)
+        {
+            Console.WriteLine($"Please enter a {word}: ");
+           string input = Console.ReadLine();
+            return input;
+        }
+
+       
+
+        public static string Method5(string firstName, string lastName)
+        {
+            string fullName = lastName + ", " + firstName;
+            Console.WriteLine("Expected outcome:" + fullName);
+            return fullName;
+        }
+
+    }
+        
+        }
+    
+    
+
